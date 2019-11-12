015ea54ed1c03262a65ee3883372610eba8cdfbd
Phil Killewald <PKillewald@mathematica-mpr.com>
2019-11-11 22:30:11 -0500

    adding schedule lookup via Firestore

--- schedule.go
+++ schedule.go
@@ -2,74 +2,87 @@ package pickem
 
 import (
 	"context"
-	"fmt"
+	"sort"
+	"strconv"
 
 	"cloud.google.com/go/firestore"
+	"google.golang.org/api/iterator"
 )
 
-// TeamSchedule is a Team's schedule for a Season.
-type TeamSchedule struct {
-	Team      *firestore.DocumentRef   `json:"team" firestore:"team"`
-	Opponents []*firestore.DocumentRef `json:"opponents" firestore:"opponents"`
-	Locations []RelativeLocation       `json:"locations" firestore:"locations"`
-}
+// byWeek sorts a collection of games by week
+type byWeek []*Game
 
-// Schedule is a complete schedule for all teams for a Season.
-type Schedule struct {
-	Season    *firestore.DocumentRef `json:"season" firestore:"season"`
-	Schedules []TeamSchedule         `json:"schedules" firestore:"schedules"`
-}
+func (b byWeek) Len() int           { return len(b) }
+func (b byWeek) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b byWeek) Less(i, j int) bool { return b[i].Week < b[j].Week }
 
-// Matchups converts a TeamSchedule into Matchups.
-func (ts *TeamSchedule) Matchups(ctx context.Context, fs *firestore.Client) ([]*Matchup, error) {
-	if len(ts.Opponents) != len(ts.Locations) {
-		return nil, fmt.Errorf("opponents and locations must be the same length")
+// Matchups converts a team's schedule into Matchups.
+func Matchups(ctx context.Context, fs *firestore.Client, team string, season int, startWeek int) ([]*Matchup, error) {
+	var t *Team
+	var err error
+	if t, err = LookupTeam(ctx, fs, team); err != nil {
+		return nil, err
 	}
-	teamDoc, err := ts.Team.Get(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("unable to get team %s from firestore: %v", ts.Team.ID, err)
-	}
-	var t1 Team
-	err = teamDoc.DataTo(&t1)
-	if err != nil {
-		return nil, fmt.Errorf("unable to unmarshal team %s: %v", ts.Team.ID, err)
-	}
-	oppDocs, err := fs.GetAll(ctx, ts.Opponents)
-	if err != nil {
-		return nil, fmt.Errorf("unable to get opponents from firestore: %v", err)
-	}
-	mus := make([]*Matchup, len(oppDocs))
-	for i, doc := range oppDocs {
-		var t2 Team
-		err = doc.DataTo(&t2)
-		if err != nil {
-			return nil, fmt.Errorf("unable to unmarshal team %s: %v", doc.Ref.ID, err)
+
+	seasonRef := fs.Collection("seasons").Doc(strconv.Itoa(season))
+	teamRef := fs.Collection("xteams").Doc(t.SchoolName)
+
+	games := make([]*Game, 0)
+	homeGameItr := fs.Collection("xgames").Where("season", "==", seasonRef).Where("week", ">=", startWeek).Where("home_team", "==", teamRef).Documents(ctx)
+	defer homeGameItr.Stop()
+	for {
+		doc, err := homeGameItr.Next()
+		if err == iterator.Done {
+			break
 		}
-		mus[i] = &Matchup{Team1: &t1, Team2: &t2, Location: ts.Locations[i]}
+		var game Game
+		if err := doc.DataTo(&game); err != nil {
+			return nil, err
+		}
+		games = append(games, &game)
 	}
-	return mus, nil
-}
 
-// MatchupMap converts a Schedule into a team => Matchup map.
-func (s *Schedule) MatchupMap(ctx context.Context, fs *firestore.Client) (map[*Team][]*Matchup, error) {
-	mm := make(map[*Team][]*Matchup)
+	awayGameItr := fs.Collection("xgames").Where("season", "==", seasonRef).Where("week", ">=", startWeek).Where("away_team", "==", teamRef).Documents(ctx)
+	defer awayGameItr.Stop()
+	for {
+		doc, err := awayGameItr.Next()
+		if err == iterator.Done {
+			break
+		}
+		var game Game
+		if err := doc.DataTo(&game); err != nil {
+			return nil, err
+		}
+		games = append(games, &game)
+	}
 
-	for _, ts := range s.Schedules {
-		mus, err := ts.Matchups(ctx, fs)
+	sort.Sort(byWeek(games))
+	matchups := make([]*Matchup, len(games))
+	for i, game := range games {
+		var t1, t2 Team
+		doc, err := game.HomeTeam.Get(ctx)
 		if err != nil {
 			return nil, err
 		}
-		if len(mus) == 0 {
-			continue
+		if err := doc.DataTo(&t1); err != nil {
+			return nil, err
 		}
-		t1 := mus[0].Team1
-		if _, ok := mm[t1]; ok {
-			return nil, fmt.Errorf("team %s appears more than once in schedules for season %s", t1.Name(), s.Season.ID)
+		doc, err = game.AwayTeam.Get(ctx)
+		if err != nil {
+			return nil, err
 		}
-		mm[t1] = mus
+		if err := doc.DataTo(&t2); err != nil {
+			return nil, err
+		}
+		loc := Home
+		if game.NeutralSite {
+			loc = Neutral
+		}
+		matchups[i] = &Matchup{Team1: &t1, Team2: &t2, Location: loc}
 	}
 
-	return mm, nil
+	return matchups, nil
+
 }
 
 func loc(locTeam string) RelativeLocation {
--- team.go
+++ team.go
@@ -1,6 +1,12 @@
 package pickem
 
-import "cloud.google.com/go/firestore"
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/iterator"
+)
 
 // A Team in Pick 'Em terms is an entity that participates in Matchups.  Teams have many representations:
 // Names are a names that are used in Excel-style slates or by NCAA prediction models, and are determined by the whims of the authors;
@@ -27,4 +33,53 @@ func (t *Team) Name() string {
 // ShortName implements NameStringer interface.
 func (t *Team) ShortName() string {
 	return t.Abbreviation
+}
+
+// LookupTeam looks up a team in Firestore by, in order, ID==SchoolName, Abbreviation, then Names.  If multiple teams match, an error is returned.
+func LookupTeam(ctx context.Context, fs *firestore.Client, name string) (*Team, error) {
+	collection := fs.Collection("xteams")
+	var team Team
+
+	doc, err := collection.Doc(name).Get(ctx)
+	if err != nil {
+		if err := doc.DataTo(&team); err != nil {
+			return nil, err
+		}
+		return &team, nil
+	}
+
+	itr := collection.Where("Abbreviation", "==", name).Documents(ctx)
+	defer itr.Stop()
+	for {
+		doc, err := itr.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err := doc.DataTo(&team); err != nil {
+			return nil, err
+		}
+		if _, err := itr.Next(); err != iterator.Done {
+			return nil, fmt.Errorf("ambiguous team abbreviation '%s'", name)
+		}
+		return &team, nil
+	}
+	itr.Stop()
+
+	itr = collection.Where("Names", "array-contains", name).Documents(ctx)
+	defer itr.Stop()
+	for {
+		doc, err := itr.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err := doc.DataTo(&team); err != nil {
+			return nil, err
+		}
+		if _, err := itr.Next(); err != iterator.Done {
+			return nil, fmt.Errorf("ambiguous team name '%s'", name)
+		}
+		return &team, nil
+	}
+
+	return nil, fmt.Errorf("name '%s' not found in teams", name)
 }
